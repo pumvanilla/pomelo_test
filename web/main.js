05ddc78efb982d302c21a9054448550a2dc7e7e9
tsoonthornvi <thanyawan.soonthornvipat@agoda.com>
2021-07-06 04:01:25 +0700

    u

--- web/main.js
+++ web/main.js
@@ -1,17 +1,19 @@
 const config = require('./config.json')
-const psql = require('pg')
+const { Client } = require('pg')
 const winston = require('winston')
 const WinstonCloudWatch = require('winston-cloudwatch');
+
+const express = require('express')
 
 // aws-sdk will not set default region
 // config ~/.aws/credentials first
 winston.add(new WinstonCloudWatch({
-  logGroupName: 'test_terraform',
+  logGroupName: 'my-app',
   logStreamName: 'log_stream1',
   awsRegion: 'ap-southeast-1'
 }));
 
-const client = new psql({
+const client = new Client({
   user: config.rds_username.value,
   host: config.rds_hostname.value,
   port: config.rds_port.value,
@@ -19,63 +21,74 @@ const client = new psql({
   database: 'testdb',
 }) 
 
-client.connect(err => {
-  if (err) throw err;
-  winston.log("DB connected!");
-})
+const initDatabase = async () => {
+  await client.connect()
 
-/* CREATE DB */
-connection.query("CREATE DATABASE test_db", function (err, result) {
-  if (err) {
-    winston.log('Cannot create DB')
-    return
-  }
-  
-  winston.log("Database created");
-});
+  await client.query("CREATE DATABASE test_db")
+
+  await client.query("CREATE TABLE users (email varchar,firstName varchar,lastName varchar,age int)");
+
+  await client.query("INSERT INTO users (email, firstName, lastName, age) VALUES ('johndoe@gmail.com', 'john', 'doe', 21)")
+}
+
+// client.connect(err => {
+//   if (err) throw err;
+//   winston.log("DB connected!");
+// })
+
+// /* CREATE DB */
+// client.query("CREATE DATABASE test_db", function (err, result) {
+//   if (err) {
+//     winston.log('Cannot create DB')
+//     return
+//   }
+//   winston.log("Database created");
+// });
 
 
 /* CREATE TABLE */
-var sql = "CREATE TABLE users (email varchar,firstName varchar,lastName varchar,age int)";
-connection.query(sql, function (err, result) {
-  if (err) {
-    winston.log('Cannot create table')
-    return
-  }
-  winston.log("Table created");
-});
+// var sql = "CREATE TABLE users (email varchar,firstName varchar,lastName varchar,age int)";
+// client.query(sql, function (err, result) {
+//   if (err) {
+//     winston.log('Cannot create table')
+//     return
+//   }
+//   winston.log("Table created");
+// });
 
 
 /* INSERT */
-var sql = "INSERT INTO users (email, firstName, lastName, age) VALUES ('johndoe@gmail.com', 'john', 'doe', 21)";
-connection.query(sql, function (err, result) {
-  if (err) throw err;
-  winston.log("1 record inserted");
-});
+// var sql = "INSERT INTO users (email, firstName, lastName, age) VALUES ('johndoe@gmail.com', 'john', 'doe', 21)";
+// client.query(sql, function (err, result) {
+//   if (err) throw err;
+//   winston.log("1 record inserted");
+// });
 
+initDatabase()
+  .then(() => {
+    const app = express()
 
-
-/* WEB SERVER */
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-  client.query('SELECT * FROM users', function (err, rows) {
-    if (err) {
-      res.json({
-        result: 'error',
-        message: 'unable to query database'
+    app.get('/', (req, res) => {
+      client.query('SELECT * FROM users', function (err, rows) {
+        if (err) {
+          res.json({
+            result: 'error',
+            message: 'unable to query database'
+          })
+        } else {
+          res.json({
+            result: 'ok',
+            data: rows[0]
+          })
+        }
       })
-    } else {
-      res.json({
-        result: 'ok',
-        data: rows[0]
-      })
-    }
+    })
+    
+    
+    app.listen(80, () => {
+      winston.log(`Example app listening at http://localhost:80`)
+    })
   })
-})
-
-
-app.listen(80, () => {
-  winston.log(`Example app listening at http://localhost:80`)
-})
+  .catch((err) => {
+    winston.error(err)
+  })
