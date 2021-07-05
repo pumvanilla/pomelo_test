123d38279496fa824d10240dec21249bc6c6fcfa
tsoonthornvi <thanyawan.soonthornvipat@agoda.com>
2021-07-06 03:30:59 +0700

    u

--- script/setup.sh
+++ script/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 sudo yum update -y
+
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+
 sudo yum install -y nodejs
 
 sudo npm install yarn -g
